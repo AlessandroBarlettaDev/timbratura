15fece60c0f5dc888681b0082c5b307cd99285f0
AlessandroBarlettaDev <137823622+AlessandroBarlettaDev@users.noreply.github.com>
2026-04-22 01:24:35 +0200

    chore: update gitignore

--- frontend/src/app/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  region:           'eu-west-1',
-  UserPoolId:       'eu-west-1_d342DZAGn',
-  UserPoolClientId: '6t804sdh72ds4t2jbmf6t386g4',
-  ApiUrl:           'https://cgohrg9hm9.execute-api.eu-west-1.amazonaws.com/prod',
-};
